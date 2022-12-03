343f82bea53ab21ae28102a1a2caf928d215c63a
NatiG100 <kalebok39@gmail.com>
2022-12-03 02:35:47 -0800

    create divider component

---
+++ src/components/uiElements/Divider/DividerElements.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const StyledDivider = styled.div`
+    ${({direction, width, height})=>(direction==="horizontal"?`
+        border-top: 1px solid #fff2;
+        width: ${width};
+    `:`
+        border-left: 1px solid #fff4;
+        height: ${height} ;
+    `)}
+`;
