bf46157fcab3a110b0938462e77f932419ade343
NatiG100 <natnaelgashu2022@gmail.com>
2022-12-11 06:09:21 -0800

    create hook for detecting intersection

---
+++ src/hooks/useIsInViewPort.js
@@ -0,0 +1,19 @@
+import { useEffect, useMemo, useState } from "react"
+
+const useIsInviewPort = (ref)=>{
+    const [isIntersecting, setIsIntersecting] = useState(false);
+
+    const observer = useMemo(
+        ()=> new IntersectionObserver(([entry])=>setIsIntersecting(entry.isIntersecting))
+        ,[]
+    );
+
+    useEffect(()=>{
+        observer.observe(ref.current);
+
+        return ()=>observer.disconnect
+    },[ref,observer]);
+
+    return isIntersecting;
+}
+export default useIsInviewPort;
