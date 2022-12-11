f1daec87f567a733ed4f00bea6f2317be8994c9a
NatiG100 <natnaelgashu2022@gmail.com>
2022-12-11 07:12:29 -0800

    fix animation logic on skill

--- src/components/Skill/index.jsx
+++ src/components/Skill/index.jsx
@@ -8,22 +8,32 @@ import {
 
 import {useSpring, config} from 'react-spring';
 import useIsInviewPort from '../../hooks/useIsInViewPort';
+import { useEffect } from 'react';
 
 const Skill = ({title="Title", number="0", level=5}) => {
     const skillRef = useRef(null);
     const isIntersecting = useIsInviewPort(skillRef);
-    const animation = useSpring({
+    const [animation,api] = useSpring(()=>({
         from:{
+            y:50,
             opacity: 0,
-            transform:"translateY(50px)",
-        },
-        to:{
-            opacity: 1,
-            transform:"translateY(0px)",
-        },
-        reset: !isIntersecting,
-        pause: !isIntersecting,
-    })
+        }
+    }));
+
+    useEffect(()=>{
+        if(isIntersecting){
+            api({
+                y:0,
+                opacity:1,
+            });
+        }
+        return ()=>{
+            api({
+                y:50,
+                opacity:0,
+            });
+        }
+    },[isIntersecting, api])
     return (
       <StyledSkillContainer ref={skillRef} style={animation}>
           <StyledSkillTitle><em>#{number}</em> {title}</StyledSkillTitle>
