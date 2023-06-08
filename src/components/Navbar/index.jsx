7f93007b6bf20eecb2b9870d9bb18e9a95ba2ef7
NatiGK <natnaelgashu2022@gmail.com>
2023-06-08 11:31:34 +0300

    add cv link

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,36 +1,82 @@
-import React from 'react';
-import Button from '../uiElements/Button';
-import {RiMenu4Fill} from 'react-icons/ri';
+import React from "react";
+import Button from "../uiElements/Button";
+import { RiMenu4Fill } from "react-icons/ri";
 import {
-    StyledNavItem,
-    StyledNavItemContainer,
-    StyledNavbar,
-    StyledNavbarWrapper,
-    StyledMenu
-} from './NavbarElements';
-import {ReactComponent as Logo} from './../../assets/svg/logo.svg';
+  StyledNavItem,
+  StyledNavItemContainer,
+  StyledNavbar,
+  StyledNavbarWrapper,
+  StyledMenu,
+} from "./NavbarElements";
+import { ReactComponent as Logo } from "./../../assets/svg/logo.svg";
 
-import { animateScroll } from 'react-scroll';
+import { animateScroll } from "react-scroll";
 
-const Navbar = ({show=true, showShadow=false, onMenuClick=()=>{}}) => {
+const Navbar = ({
+  show = true,
+  showShadow = false,
+  onMenuClick = () => {},
+}) => {
   return (
     <StyledNavbar show={show} showShadow={showShadow}>
-        <StyledNavbarWrapper>
-            <Logo onClick={animateScroll.scrollToTop}/>
-            <StyledMenu onClick={onMenuClick}>
-              <RiMenu4Fill/>
-            </StyledMenu>
-            <StyledNavItemContainer>
-                <StyledNavItem activeClass="activeNavItem" spy={true}smooth={true} exact="true" to="about" ><em># </em>about</StyledNavItem>
-                <StyledNavItem activeClass="activeNavItem" spy={true}smooth={true} exact="true" to="skills" ><em># </em>skills</StyledNavItem>
-                <StyledNavItem activeClass="activeNavItem" spy={true}smooth={true} exact="true" to="experience" ><em># </em>experience</StyledNavItem>
-                <StyledNavItem activeClass="activeNavItem" spy={true}smooth={true} exact="true" to="projects" ><em># </em>projects</StyledNavItem>
-                <StyledNavItem activeClass="activeNavItem" spy={true}smooth={true} exact="true" to="contact" ><em># </em>contact</StyledNavItem>
-                <Button>Resume</Button>
-            </StyledNavItemContainer>
-        </StyledNavbarWrapper>
+      <StyledNavbarWrapper>
+        <Logo onClick={animateScroll.scrollToTop} />
+        <StyledMenu onClick={onMenuClick}>
+          <RiMenu4Fill />
+        </StyledMenu>
+        <StyledNavItemContainer>
+          <StyledNavItem
+            activeClass="activeNavItem"
+            spy={true}
+            smooth={true}
+            exact="true"
+            to="about"
+          >
+            <em># </em>about
+          </StyledNavItem>
+          <StyledNavItem
+            activeClass="activeNavItem"
+            spy={true}
+            smooth={true}
+            exact="true"
+            to="skills"
+          >
+            <em># </em>skills
+          </StyledNavItem>
+          <StyledNavItem
+            activeClass="activeNavItem"
+            spy={true}
+            smooth={true}
+            exact="true"
+            to="experience"
+          >
+            <em># </em>experience
+          </StyledNavItem>
+          <StyledNavItem
+            activeClass="activeNavItem"
+            spy={true}
+            smooth={true}
+            exact="true"
+            to="projects"
+          >
+            <em># </em>projects
+          </StyledNavItem>
+          <StyledNavItem
+            activeClass="activeNavItem"
+            spy={true}
+            smooth={true}
+            exact="true"
+            to="contact"
+          >
+            <em># </em>contact
+          </StyledNavItem>
+          <a href="/cv.pdf" target="_blank">
+            <Button>Resume</Button>
+          </a>
+        </StyledNavItemContainer>
+      </StyledNavbarWrapper>
     </StyledNavbar>
-  )
-}
+  );
+};
 
 export default Navbar;
